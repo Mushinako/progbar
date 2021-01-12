fc78b02faadce876edd1407fb9e9cb109e1a1b11
Mushinako <8977737+Mushinako@users.noreply.github.com>
2021-01-11 16:13:07 -0800

    Stub file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fp:
 
 setuptools.setup(
     name="progbar-mushinako",  # Replace with your own username
-    version="1.0",
+    version="1.1",
     author="Mushinako",
     author_email="ridoedee@gmail.com",
     description="Simple progress bar for personal use",
